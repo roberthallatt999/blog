df12747f1f254ba38831626d96ec355530d8c12b
roberthallatt <roberth@cps.ca>
2023-12-28 22:12:06 -0500

    Optimize files

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,3 @@
 # build Jekyll for production
 
 JEKYLL_ENV=production bundle exec jekyll b
-
-# commit changes to Git and use deploy hook to push changes to the server
-
-git add .
-read -p 'Commit message: ' message
-git commit -m "$message"
-git push
-git push production master
